3a028e923d1ce82b46d5cb5bc38e8d4b7a898f13
Teagan glenn <that@teagantotally.rocks>
2025-06-11 06:31:40 +0000

    Pipeline stage

--- internal/pipeline/stage.go
+++ internal/pipeline/stage.go
@@ -1,11 +1,11 @@
 package pipeline
 
 type PipelineStage[IN any, OUT any] struct {
-	Name      string                     // Name of the stage, used for identification and logging
-	Arguments <-chan IN                  // Input channel for the stage, where data is received
-	Output    chan<- *OUT                // Output channel for the stage, where processed data is sent
-	Error     chan<- error               // Error channel for the stage, where errors are sent
-	Next      *[]PipelineStage[OUT, any] // Pointer to the next stage in the pipeline, allowing chaining of stages
+	Name   string                     // Name of the stage, used for identification and logging
+	Input  <-chan IN                  // Input channel for the stage, where data is received
+	Output chan<- *OUT                // Output channel for the stage, where processed data is sent
+	Error  chan<- error               // Error channel for the stage, where errors are sent
+	Next   *[]PipelineStage[OUT, any] // Pointer to the next stage in the pipeline, allowing chaining of stages
 }
 
 type PipelineStageInterface[IN any, OUT any] interface {
